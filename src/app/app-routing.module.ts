cdfa9e49a7488afefc29eb9bb792c76d346379a2
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-11-02 16:12:59 +0300

    finish the UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaseComponent } from './case/case.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PoliceComponent } from './police/police.component';
+import { ReviewComponent } from './review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'police', component: PoliceComponent},
+  {path: 'case', component: CaseComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'reviews', component: ReviewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PoliceComponent } from './police/police.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CaseComponent } from './case/case.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PoliceComponent,
+    NavbarComponent,
+    FooterComponent,
+    CaseComponent,
+    ContactComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
